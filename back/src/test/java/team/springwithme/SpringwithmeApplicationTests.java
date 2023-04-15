08cae8e25e94c444f40454c75e5064423fd45ac8
moonyoungho <ansdudgh98@naver.com>
2023-04-15 13:00:47 +0900

    chore : "프로젝트 init" #6

---
+++ back/src/test/java/team/springwithme/SpringwithmeApplicationTests.java
@@ -0,0 +1,13 @@
+package team.springwithme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
