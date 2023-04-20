7e7a1b869ede8b916e42b71ef60c5c2ceea717f9
moonyoungho <ansdudgh98@naver.com>
2023-04-20 10:11:29 +0900

    feat : "세션 기능 개발 및 argument resolver 추가"

---
+++ src/main/java/team/studywithme/config/session/SessionArgumentResolver.java
@@ -0,0 +1,27 @@
+package team.studywithme.config.session;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Component
+public class SessionArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Session.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        HttpSession session = request.getSession();
+        return session.getAttribute("session");
+    }
+}
