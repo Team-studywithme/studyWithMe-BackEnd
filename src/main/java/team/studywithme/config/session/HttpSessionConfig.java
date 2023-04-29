af3c8b8a030f44e14f35bb138034d429a4232227
H-S Cha <xcohdsuts@gmail.com>
2023-04-29 14:10:09 +0900

    refactor : "Session Config TransactionManager Bean 명시" #26

--- src/main/java/team/studywithme/config/session/HttpSessionConfig.java
+++ src/main/java/team/studywithme/config/session/HttpSessionConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -18,5 +20,9 @@ public class HttpSessionConfig {
                 .setType(EmbeddedDatabaseType.H2)
                 .addScript("org/springframework/session/jdbc/schema-h2.sql")
                 .build();
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
     }
 }
