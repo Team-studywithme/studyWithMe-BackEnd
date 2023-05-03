a2f9f108e91c49e9f45479c84631b4291874cd56
chahyoungseok <xcohdsuts@gmail.com>
2023-05-03 15:01:25 +0900

    refactor : "테스트 시 통합 기본 데이터 제거" #29

--- src/test/resources/sql/data-test.sql
+++
@@ -1,5 +0,0 @@
------------------------------- Avatar Data -----------------------------
-INSERT INTO avatar(id, active, created_date, last_modified_date, nickname) values (null, 1, '2023-04-11T11:44:30', '2023-04-13T09:22:31', 'beforeNickname')
-
------------------------------- Account Data ------------------------------
-INSERT INTO account(id, active, created_date, last_modified_date, email, avatar_id) values ('2753601544', 1, '2023-04-11T11:44:30', '2023-04-13T09:22:31', 'weihyuk39@nate.com', 1)
