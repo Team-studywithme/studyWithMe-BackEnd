805952ff694ebeef43c931825388b8712d838654
chahyoungseok <xcohdsuts@gmail.com>
2023-05-03 09:51:18 +0900

    feature : "게시물 관련 쿼리 수정 및 추가" #23

--- src/main/java/team/studywithme/repository/CommentRepository.java
+++ src/main/java/team/studywithme/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package team.studywithme.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,12 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
             "where co.post.id = :postID and co.active = 1 " +
             "order by co.createdDate desc")
     Slice<Comment> findSliceComments(Pageable pageable, @Param("postID") Long postID);
+
+    @Query(value = "select co from Comment co " +
+            "where co.id = :commentID and co.active = 1")
+    Comment findCommentById(@Param("commentID") Long commentID);
+
+    @Modifying
+    @Query(value = "update Comment co set co.active = 0 where co.post.id = :postID ")
+    int deleteByPost(@Param("postID") Long postID);
 }
