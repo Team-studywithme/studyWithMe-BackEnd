e344a7da697d32ea1b0b5bc31294403223bd972c
chahyoungseok <xcohdsuts@gmail.com>
2023-05-03 15:00:44 +0900

    feature : "Service단 테스트 스트럭쳐 설계" #29

---
+++ src/test/java/team/studywithme/service/structure/BoardDataServiceTest.java
@@ -0,0 +1,34 @@
+package team.studywithme.service.structure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import team.studywithme.domain.entity.Avatar;
+import team.studywithme.domain.entity.Board;
+import team.studywithme.domain.entity.Post;
+import team.studywithme.repository.BoardRepository;
+import team.studywithme.repository.PostRepository;
+
+import java.util.List;
+
+public class BoardDataServiceTest extends UserDataServiceTest{
+
+    @Autowired
+    public BoardRepository boardRepository;
+    @Autowired
+    public PostRepository postRepository;
+
+    public Board makeBoard(){
+        return boardRepository.save(new Board("matching"));
+    }
+
+    public Post makePost(Avatar avatar, Board board){
+        return postRepository.saveAndFlush(new Post(avatar, board, 0, "title_A", "content_A"));
+    }
+
+    public List<Post> makePostList(Avatar avatar, Board board){
+        return postRepository.saveAllAndFlush(List.of(
+                new Post(avatar, board, 0, "title_A", "content_A"),
+                new Post(avatar, board, 0, "title_B", "content_B"),
+                new Post(avatar, board, 0, "title_C", "content_C")
+        ));
+    }
+}
---
+++ src/test/java/team/studywithme/service/structure/PostDataServiceTest.java
@@ -0,0 +1,24 @@
+package team.studywithme.service.structure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import team.studywithme.domain.entity.Avatar;
+import team.studywithme.domain.entity.Comment;
+import team.studywithme.domain.entity.Post;
+import team.studywithme.repository.CommentRepository;
+
+import java.util.List;
+
+public class PostDataServiceTest extends BoardDataServiceTest{
+
+    @Autowired
+    public CommentRepository commentRepository;
+
+    public List<Comment> makeCommentList(Avatar avatar, Post post){
+        return commentRepository.saveAllAndFlush(List.of(
+                new Comment(avatar, post, "content_1"),
+                new Comment(avatar, post, "content_2"),
+                new Comment(avatar, post, "content_3")
+        ));
+    }
+
+}
