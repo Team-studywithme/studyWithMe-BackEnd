fc3648216d1962c851b471181f6f0dd45e292e91
chahyoungseok <xcohdsuts@gmail.com>
2023-05-25 19:26:38 +0900

    refactor : "Interceptor 예외처리" #10

--- src/main/java/team/studywithme/api/controller/interceptor/LoginInterceptor.java
+++ src/main/java/team/studywithme/api/controller/interceptor/LoginInterceptor.java
@@ -2,7 +2,9 @@ package team.studywithme.api.controller.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -12,12 +14,13 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        HttpSession session = request.getSession();
+        HttpSession session = request.getSession(false);
 
         if(session == null || session.getAttribute("session") == null){
-            return false;
+            throw new AuthenticationException("세션값이 없습니다.");
         }
 
         return true;
     }
+
 }
